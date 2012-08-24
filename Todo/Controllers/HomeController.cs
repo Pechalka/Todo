7bde8d22da526246d4691235e6ea9b247b760bb2
Andrew <luzkovskyandrey@gmail.com>
2012-08-24 10:27:03 +0300

    init comit

---
+++ Todo/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using MongoDB.Driver;
+
+namespace Todo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly TaskDataService _taskDataService;
+
+        public HomeController()
+        {
+            _taskDataService = new TaskDataService();
+        }
+
+        public ActionResult Index()
+        {
+            return View(_taskDataService.GetCurrentTasks());
+        }
+
+        public ActionResult Update(List<Task> tasks)
+        {
+            _taskDataService.Update(tasks);
+            return new EmptyResult();
+        }
+
+    }
+}
+
+
+public class TaskDataService
+{
+    private readonly MongoCollection<Task> _tasks;
+
+    public TaskDataService()
+    {
+        var server = MongoServer.Create("mongodb://localhost:27020/");
+        var database = server.GetDatabase("todos");
+        _tasks = database.GetCollection<Task>("tasks");
+    }
+
+    public List<Task> GetCurrentTasks()
+    {
+        //return new List<Task>
+        //           {
+        //               new Task{ title = "test"},
+        //               new Task{ title = "test1"},
+        //               new Task{ title = "test2"},
+        //               new Task{ title = "test3"}
+        //           };
+
+        return _tasks.FindAll().ToList();
+    }
+
+    public void Update(List<Task> tasks)
+    {
+        if (tasks == null || tasks.Count == 0)
+            return; 
+        _tasks.RemoveAll();
+        _tasks.InsertBatch(tasks);
+    }
+}
+
+public class Task
+{
+    public Guid Id { get; set; }
+    public string title { get; set; }
+
+    public bool Day1 { get; set; }
+    public bool Day2 { get; set; }
+    public bool Day3 { get; set; }
+    public bool Day4 { get; set; }
+    public bool Day5 { get; set; }
+    public bool Day6 { get; set; }
+    public bool Day7 { get; set; }
+}
---
+++ Todo/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// общие сведения об этой сборке.
+[assembly: AssemblyTitle("Todo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Todo")]
+[assembly: AssemblyCopyright("© , 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми 
+// для компонентов COM.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для ID-идентификации библиотеки типов, если этот проект видим для COM
+[assembly: Guid("0e223f5a-12cf-4a8f-a9b6-81554ce5a26a")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
